fe6b62b5cfbab667b2f3dbef25ea3656d83df6d7
Crowdbotics <team@crowdbotics.com>
2024-05-19 11:31:59 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "seckapp-48018",
+  url: "https://seckapp-48018.botics.co",
+  api: "https://seckapp-48018.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
